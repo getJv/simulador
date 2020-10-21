361b25f8dc4b78461d25e42a6f4657010bdb5941
Jhonatan Morais <jhonatanvinicius@gmail.com>
2020-10-21 01:02:52 -0300

    heroku dependencies

---
+++ dist/precache-manifest.4789eb9b7dfb0a03578e84024e598fc5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cb707c04b771d124e0",
+    "url": "/css/app.6fe738ea.css"
+  },
+  {
+    "revision": "3fa4e61d9600045cd6fb",
+    "url": "/css/chunk-vendors.4d925d50.css"
+  },
+  {
+    "revision": "83332a6f275d324c714c6ea40314863d",
+    "url": "/fonts/materialdesignicons-webfont.83332a6f.eot"
+  },
+  {
+    "revision": "96d58a0792f63ec3808a897effe17faf",
+    "url": "/fonts/materialdesignicons-webfont.96d58a07.woff"
+  },
+  {
+    "revision": "c1242726c7eac4eb5e843d826f78fb1b",
+    "url": "/fonts/materialdesignicons-webfont.c1242726.ttf"
+  },
+  {
+    "revision": "ecab20e9fda0eef864b6d2ca27b44b79",
+    "url": "/fonts/materialdesignicons-webfont.ecab20e9.woff2"
+  },
+  {
+    "revision": "ee87a8f34b27a539527fa820e2323740",
+    "url": "/img/pexels-marcin-jozwiak-3634730-opaco.ee87a8f3.jpg"
+  },
+  {
+    "revision": "c7cbaa5507ae584285e55fe1f5108c31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99cb707c04b771d124e0",
+    "url": "/js/app.38f92ff5.js"
+  },
+  {
+    "revision": "3fa4e61d9600045cd6fb",
+    "url": "/js/chunk-vendors.5fd83385.js"
+  },
+  {
+    "revision": "222eb2bd8ed701cd87cdb0c19feced2e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
